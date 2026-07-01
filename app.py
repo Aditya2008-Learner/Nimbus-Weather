2a3fe4f7195ea5d1aa3bbb45ec058beb31554f58
Aditya2008-Learner <senaditya2008@gmail.com>
2026-07-01 20:49:58 +0530

    Implemented Air Quality Index and updated weather interface

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def build_weather(data):
         "description": data["weather"][0]["description"].title(),
         "icon": data["weather"][0]["icon"],
         "main": data["weather"][0]["main"],
+"lat": data["coord"]["lat"],
+"lon": data["coord"]["lon"],
 
         "sunrise": format_time(
             data["sys"]["sunrise"],
@@ -96,17 +98,46 @@ def home():
         forecast_data = forecast_response.json()
 
         if response.status_code == 200:
-
             weather = build_weather(data)
 
+            aqi_url = (
+                "https://api.openweathermap.org/data/2.5/air_pollution"
+                f"?lat={weather['lat']}"
+                f"&lon={weather['lon']}"
+                f"&appid={API_KEY}"
+            )
+
+            aqi_response = requests.get(aqi_url)
+            aqi_data = aqi_response.json()
+
+            weather["aqi"] = aqi_data["list"][0]["main"]["aqi"]
+
+            aqi_levels = {
+                1: ("Good", "#2ecc71"),
+                2: ("Fair", "#f1c40f"),
+                3: ("Moderate", "#e67e22"),
+                4: ("Poor", "#e74c3c"),
+                5: ("Very Poor", "#8e44ad")
+            }
+
+            weather["aqi_text"] = aqi_levels[weather["aqi"]][0]
+            weather["aqi_color"] = aqi_levels[weather["aqi"]][1]
+
             # Get one forecast (12 PM) for each day
+            daily_forecast = []
+
             for item in forecast_data["list"]:
-
                 if "12:00:00" in item["dt_txt"]:
-                    daily_forecast.append(item)
-
+                    daily_forecast.append({
+                        "day": datetime.strptime(
+                            item["dt_txt"],
+                            "%Y-%m-%d %H:%M:%S"
+                        ).strftime("%a"),
+                        "temp": round(item["main"]["temp"]),
+                        "description": item["weather"][0]["description"].title(),
+                        "icon": item["weather"][0]["icon"]
+                    })
         else:
-
             weather = {
                 "error": "City not found."
             }
@@ -116,7 +147,6 @@ def home():
         weather=weather,
         forecast=daily_forecast
     )
-
 
 @app.route("/location")
 def location():
@@ -138,7 +168,32 @@ def location():
     print("Response:", data)
 
     if response.status_code == 200:
-        session["weather"] = build_weather(data)
+        weather = build_weather(data)
+
+        aqi_url = (
+            "https://api.openweathermap.org/data/2.5/air_pollution"
+            f"?lat={weather['lat']}"
+            f"&lon={weather['lon']}"
+            f"&appid={API_KEY}"
+        )
+
+        aqi_response = requests.get(aqi_url)
+        aqi_data = aqi_response.json()
+
+        weather["aqi"] = aqi_data["list"][0]["main"]["aqi"]
+
+        aqi_levels = {
+            1: ("Good", "#2ecc71"),
+            2: ("Fair", "#f1c40f"),
+            3: ("Moderate", "#e67e22"),
+            4: ("Poor", "#e74c3c"),
+            5: ("Very Poor", "#8e44ad")
+        }
+
+        weather["aqi_text"] = aqi_levels[weather["aqi"]][0]
+        weather["aqi_color"] = aqi_levels[weather["aqi"]][1]
+
+        session["weather"] = weather
     else:
         session["weather"] = {
             "error": "Unable to fetch location weather."
